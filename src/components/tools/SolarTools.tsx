f484594b3275633e1b5c926f926b65e9707e60e6
TD_SoftwareEngr <tobidelly@gmail.com>
2025-03-08 20:15:15 +0100

    SolarTools Updated

--- src/components/tools/SolarTools.tsx
+++ src/components/tools/SolarTools.tsx
@@ -111,7 +111,7 @@ export default function SolarTools() {
       {/* Footer */}
       <footer className="bg-gray-900 text-white py-8">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
-          <p>© {new Date().getFullYear()} SolarTech Pro. All rights reserved.</p>
+          <p>© {new Date().getFullYear()} Bolty Technologies Limited. All rights reserved.</p>
         </div>
       </footer>
     </div>
